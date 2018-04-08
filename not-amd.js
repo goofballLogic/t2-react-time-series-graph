3b081bb171f58bc7d9fa637712563e291a709bfc
Andrew Gibson <andrew@goofballLogic.com>
2018-04-08 11:05:54 +0100

    oops

--- not-amd.js
+++
@@ -1,9 +0,0 @@
-window.define = function( deps, factory ) {
-
-    console.log( "Deps", deps );
-    console.log( "Factory", factory );
-    window.x = factory();
-    console.log( window.x );
-    
-};
-window.define.amd = "not really";
